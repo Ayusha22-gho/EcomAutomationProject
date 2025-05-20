0a4589883ebb871b960e3bf215d5e4d00baf13fe
Ayusha Ghosh <jiyaghosh879@gmail.com>
2025-05-20 23:28:43 +0530

    Parametrization using dataprovider : return hashmap

--- src/test/java/org/ecomautomation/Tests/StandAloneTest.java
+++ src/test/java/org/ecomautomation/Tests/StandAloneTest.java
@@ -8,20 +8,21 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class StandAloneTest extends BaseTest {
 
     String productName = "ZARA COAT 3";
-    @Test(dataProvider = "getData", groups ={"Purchase"})
-    public void submitOrder(String email, String pass, String productName) throws IOException {
+    @Test(dataProvider = "getHashData", groups ={"Purchase"})
+    public void submitOrder(HashMap<String,String> inputs) throws IOException {
 
-        ProductCatalogue pdp = landingPage.loginApplication(email,pass);
+        ProductCatalogue pdp = landingPage.loginApplication(inputs.get("email"),inputs.get("password"));
         List<WebElement> products = pdp.getProductList();
-        pdp.addProducToCart(productName);
+        pdp.addProducToCart(inputs.get("product"));
         CartPage cart = pdp.goToCartPage();
 
-        Boolean matchedItem = cart.getCartMatchedItem(productName);
+        Boolean matchedItem = cart.getCartMatchedItem(inputs.get("product"));
         Assert.assertTrue(matchedItem);
 
         ShippingPage ship = cart.clickCheckout();
@@ -47,5 +48,20 @@ public class StandAloneTest extends BaseTest {
                {"jiyaghosh879@gmail.com","Rules123","ADIDAS ORIGINAL"}};
 
        return data;
+    }
+
+    @DataProvider
+    public Object[][] getHashData(){
+        HashMap<String,String> map = new HashMap<>();
+        map.put("email","ayghosh977@gmail.com");
+        map.put("password","Rules123");
+        map.put("product","ZARA COAT 3");
+
+        HashMap<String,String> map1 = new HashMap<>();
+        map1.put("email","jiyaghosh879@gmail.com");
+        map1.put("password","Rules123");
+        map1.put("product","ADIDAS ORIGINAL");
+
+        return new Object[][] {{map},{map1}};
     }
 }
